7edc0051611f6c33d6d387bd34a2184cb6c4df42
YournameITManoah <manoah@yournameit.nl>
2024-12-09 08:20:55 +0100

    chore: lint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
 import globals from 'globals'
 import pluginJs from '@eslint/js'
-import tseslint from 'typescript-eslint'
+import tslint from 'typescript-eslint'
 import pluginVue from 'eslint-plugin-vue'
 import eslintConfigPrettier from 'eslint-config-prettier'
 
@@ -10,10 +10,10 @@ export default [
     { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
     {
         files: ['**/*.vue'],
-        languageOptions: { parserOptions: { parser: tseslint.parser } },
+        languageOptions: { parserOptions: { parser: tslint.parser } },
     },
     pluginJs.configs.recommended,
-    ...tseslint.configs.strict,
+    ...tslint.configs.strict,
     ...pluginVue.configs['flat/recommended'],
     eslintConfigPrettier,
     { rules: { 'no-undef': 'off' } },
