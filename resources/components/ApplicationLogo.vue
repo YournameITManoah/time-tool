e51589b770bfc770caa9d496fc58fa591b394886
YournameITManoah <manoah@yournameit.nl>
2024-12-13 15:45:05 +0100

    fix: remove your name it logo for now

--- resources/components/ApplicationLogo.vue
+++ resources/components/ApplicationLogo.vue
@@ -1,4 +1,7 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const name = import.meta.env.VITE_APP_NAME
+</script>
 <template>
-    <v-img src="/img/ynit_logo.png" :aspect-ratio="474 / 282" />
+    <!--<v-img src="/img/ynit_logo.png" :aspect-ratio="474 / 282" />-->
+    <h1 style="text-align: center">{{ name }}</h1>
 </template>
