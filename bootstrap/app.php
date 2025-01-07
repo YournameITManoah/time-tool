fdb8e453438c9da9a0af63684cb9a8345be83f4b
YournameITManoah <manoah@yournameit.nl>
2025-01-07 08:59:59 +0100

    fix: always return json for api endpoints

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->statefulApi();
+        $middleware->prependToGroup('api', [\App\Http\Middleware\ForceJsonResponse::class]);
         $middleware->appendToGroup('web', [\App\Http\Middleware\HandleHybridRequests::class, \App\Http\Middleware\Localization::class]);
     })
     ->withExceptions(function (Exceptions $exceptions) {})->create();
