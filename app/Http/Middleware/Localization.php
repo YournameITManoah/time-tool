80a31692bfa3c0c8308dee49a898e511239b0b95
YournameITManoah <manoah@yournameit.nl>
2025-01-07 11:17:02 +0100

    feat: use browser locale as default

--- app/Http/Middleware/Localization.php
+++ app/Http/Middleware/Localization.php
@@ -6,6 +6,7 @@ use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Collection;
 use Symfony\Component\HttpFoundation\Response;
 
 class Localization
@@ -19,7 +20,45 @@ class Localization
     {
         if (Session::has('locale')) {
             App::setLocale(Session::get('locale'));
+        } else {
+            $supported = array_keys(\Config::get('app.locales'));
+            $preferred = $this->parseAcceptLanguage($request);
+
+            foreach ($preferred as $value) {
+                if (in_array($value, $supported)) {
+                    App::setLocale($value);
+                    Session::put('locale', $value);
+                    break;
+                }
+            }
         }
         return $next($request);
+    }
+
+    private function parseAcceptLanguage(Request $request)
+    {
+        $list = explode(',', $request->server('HTTP_ACCEPT_LANGUAGE', ''));
+
+        $locales = Collection::make($list)
+            ->map(function ($locale) {
+                $parts = explode(';', $locale);
+
+                $mapping['locale'] = trim($parts[0]);
+
+                if (isset($parts[1])) {
+                    $factorParts = explode('=', $parts[1]);
+
+                    $mapping['factor'] = $factorParts[1];
+                } else {
+                    $mapping['factor'] = 1;
+                }
+
+                return $mapping;
+            })
+            ->sortByDesc(function ($locale) {
+                return $locale['factor'];
+            });
+
+        return $locales->map(fn($l) => $l['locale']);
     }
 }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->statefulApi();
-        $middleware->prependToGroup('api', [\App\Http\Middleware\ForceJsonResponse::class]);
-        $middleware->appendToGroup('web', [\App\Http\Middleware\HandleHybridRequests::class, \App\Http\Middleware\Localization::class]);
+        $middleware->prependToGroup('api', [\App\Http\Middleware\Localization::class, \App\Http\Middleware\ForceJsonResponse::class]);
+        $middleware->appendToGroup('web', [\App\Http\Middleware\Localization::class, \App\Http\Middleware\HandleHybridRequests::class]);
     })
     ->withExceptions(function (Exceptions $exceptions) {})->create();
